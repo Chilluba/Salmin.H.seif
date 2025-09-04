c00955a43e2a11a29ce04d57a0499a215f7cd208
Chilluba <salminhabibu2050@gmail.com>
2025-09-04 23:34:28 +0300

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -20,7 +20,7 @@ const itemVariants: Variants = {
   visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: 'easeOut' } },
 };
 
-const CHARACTER_IMAGE_URL = "";
+const CHARACTER_IMAGE_URL = "https://i.imgur.com/gBwva9K.png";
 
 export const Home: React.FC = () => {
   const { content } = useContent();
